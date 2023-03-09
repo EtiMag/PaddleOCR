d8bc96dbb495bf3242ff6613e2b252d10795e805
EtiMag <etienne.maeght@ensae.fr>
2023-03-09 17:37:35 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!bin/sh
+# note: needs admin privilege
+apt-get update && apt-get install libgl1
+apt-get install libglib2.0-0
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH=$PATH:~/.local/share/pypoetry/venv/bin
+poetry --version
+poetry install
+# exemple: poetry run paddleocr --image_dir ticket-de-caisse-5.png --use_gpu false --lang en
