8edd562508c651eb6887d1447f65046285277637
EtiMag <etienne.maeght@ensae.fr>
2023-03-13 14:11:18 +0000

    Update setup and readme

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-#!bin/sh
-# note: needs admin privilege
-apt-get update && apt-get install libgl1
-apt-get install libglib2.0-0
+#!/bin/bash
+sudo apt-get update && apt-get -y install libgl1
+sudo apt-get -y install libglib2.0-0
 curl -sSL https://install.python-poetry.org | python3 -
-export PATH=$PATH:~/.local/share/pypoetry/venv/bin
-poetry --version
+echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
+export PATH=$PATH:$HOME/.local/bin
+echo "output of poetry --version = (poetry --version)"
 poetry install
-# exemple: poetry run paddleocr --image_dir test-files/ticket-de-caisse-5.png --use_gpu false --lang en
---
+++ readme.md
@@ -0,0 +1,18 @@
+#### Setup the environment:
+Assumed environment is the docker image defined [here](https://github.com/InseeFrLab/images-datascience/tree/main/python-datascience), within the Onyxia catalog.
+1. Clone the repository:
+    ```
+    git clone https://$GIT_USER_NAME:$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/EtiMag/PaddleOCR.git
+    cd PaddleOCR
+    ```
+    
+2. Run setup file
+    ```
+    chmod +x install.sh
+    ./install.sh && source ~/.bashrc
+    ```
+
+3. Test PaddleOCR on an image
+    ```
+    poetry run paddleocr --image_dir test-files/ticket-de-caisse-5.png --use_gpu false --lang en
+    ```
