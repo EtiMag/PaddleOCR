49a37af6a03b5ce532c989b614990af8c34d146e
EtiMag <etienne.maeght@ensae.fr>
2023-03-30 12:49:16 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt-get -y install libglib2.0-0
 curl -sSL https://install.python-poetry.org | python3 -
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 export PATH=$PATH:$HOME/.local/bin
-echo "output of poetry --version = (poetry --version)"
+echo "output of poetry --version = $(poetry --version)"
 poetry install
